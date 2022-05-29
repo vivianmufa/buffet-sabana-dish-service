5cb485f5ee9d978caac8b78ae20916874d393a93
Vivian Mui <62396706+vivianmufa@users.noreply.github.com>
2022-05-28 19:42:39 -0700

    Update CORS

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,17 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  app.enableCors({
+    origin: [
+      'http://localhost:3000',
+    ],
+    methods: ["GET", "POST", "DELETE", "PATCH", "PUT"],
+    credentials: true,
+  });
+  app.useGlobalPipes(new ValidationPipe({whitelist: true, transform: true}))
+  await app.listen(4000);
 }
 bootstrap();
